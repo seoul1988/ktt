f788ea4f022aa9fe9c9c5ff1d718ea53f65facf7
Daniel Lee <fcbs2023@gmail.com>
2026-07-20 15:06:25 -0400

    Update

---
+++ app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "KTown Triangle",
+
+    short_name: "KTT",
+
+    start_url: "/",
+
+    display: "standalone",
+
+    background_color: "#F8F3EC",
+
+    theme_color: "#172033",
+
+    icons: [
+      {
+        src: "/icon-512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
